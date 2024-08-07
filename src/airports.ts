3167f7d766f4dfc8c1b9176e66ee785a815c6f07
Johannes Kinast <5927337+undefined@users.noreply.github.com>
2024-08-07 15:03:42 +0000

    Fix search

--- src/airports.ts
+++ src/airports.ts
@@ -2,15 +2,15 @@ import BaseElement from './base';
 
 export default class extends BaseElement {
   _selectTemplate(item) {
-    return item.original.iatacode;
+    return item.original.iatacode ? item.original.iatacode : item.original.icaocode;
   }
 
   _lookup(item, mentionText) {
-    return item.name + ' ' + item.country + ' ' + item.locode;
+    return item.name + ' ' + item.country + ' ' + item.iatacode + ' ' + item.icaocode;
   }
 
   _menuItemTemplate(item) {
-    return item.original.name + ', ' + item.original.country + ' [' + item.original.iatacode + ']';
+    return item.original.name + ', ' + item.original.country + ' [' + (item.original.iatacode ? item.original.iatacode : item.original.icaocode) + ']';
   }
 
   _values(text, cb) {
